dc807543268c2c4b49bf96d600aff6555a65481c
Bruno Montebello <brunormontebello@hotmail.com>
2021-07-02 22:11:21 +0100

    Seed the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Giftcard.create!(number: '1234567891234567', password: '123456', expiration: Date.today + 8.months, message: 'Feliz aniversario')
+Giftcard.create!(number: '1234567851234567', password: '123456', expiration: Date.today + 8.months, message: 'Feliz aniversario')
 Giftcard.create!(number: '9244567891334517', password: '123456', expiration: Date.today + 5.years, message: 'Feliz casamento')
